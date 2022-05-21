37da85ad51fd6b80521d5ccb2e5dc024a00ea0a4
Nagesh2502 <maddiletinagesh72@gmail.com>
2022-05-21 16:00:22 +0000

    Midifed the file to remote repo

--- dbutiles.java
+++ dbutiles.java
@@ -1,3 +1,3 @@
-it contains the db connections
+it contains the modify the db connections
 first it will in working area and then staging are and then local repository and then remote reository using git cmds
 
